14c9c0ccdff8773c5dc00f99d920be18210bf19d
wellington.com <167912076+wellingtonbike@users.noreply.github.com>
2024-11-13 11:56:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'Java é uma linguagem de programação e plataforma de computação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Paris?',
+    'Paris não tem capital, por ser a capital da França'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Bom dia em Inglês?',
+    'Bom dia em ingles é good morning'
+
+)
